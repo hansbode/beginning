25c72a69cb1460efe4e3469db0df741747d47739
TEC\HCBS <hans.bode@tecalliance.net>
2023-09-13 11:28:40 +0200

    Tipo de datos

--- Program.cs
+++ Program.cs
@@ -1,23 +1,53 @@
-﻿namespace HelloWorld
+﻿using System.Globalization;
+using System.Runtime.CompilerServices;
+using System.Threading.Channels;
+
+namespace HelloWorld
 {
     class Program
     {
         static void Main(string[] args)
         {
-           Console.WriteLine("Gebe mir eine Zahl");
-           string ersteZahl = Console.ReadLine();
-
-
-           Console.WriteLine("Gebe mir ein andere Zahl");
-           string zweiterZahl = Console.ReadLine();
-
-           int nummer1 = Convert.ToInt16(ersteZahl);
-           int nummer2 = Convert.ToInt16(zweiterZahl);
+            //CultureInfo cu = new CultureInfo("en-us");                         esto es para que la coma se vuelva punto en idioma gringo
+            //CultureInfo.CurrentCulture = cu;
+            string Text = "Esto es freestyle";
+            Console.WriteLine(Text);
+            int zahl = 17;
+            Console.WriteLine(zahl);
+            double kommazahl =21.3;
+            Console.WriteLine(kommazahl);
+            float weiterFliesKomma = 24.34345345F;
+            Console.WriteLine(weiterFliesKomma); 
+            decimal flieskomma = 26.5M; 
+            Console.WriteLine(flieskomma);
            
+            bool wahrheitswert =true;
+            Console.WriteLine(wahrheitswert);
+            bool anders = false;
+            Console.WriteLine(anders);
 
-           int resultado = nummer1 - nummer2;
+           char buchstabe = (char)42; 
+           Console.WriteLine(buchstabe);
 
-           Console.WriteLine($"Du hast zu erst {ersteZahl} danach {zweiterZahl} und die Subtraktion zwischen beide ist {resultado} ");
+           long langezahl = 1234567891012345;
+           Console.WriteLine(langezahl);
+
+            uint positiveZahlen= 123;
+            Console.WriteLine(positiveZahlen);
+
+
+
+            string[] myArray = new string[10];
+            myArray[0] = "erster Wert";
+            myArray[9] = "letzter Wert";
+
+            List<string> myList = new List<string>();
+            
+            myList.Add("Hans");
+            myList.Add("Samayoa");
+
+            Console.WriteLine(myList[0]);
+            Console.WriteLine(myList[1]);
 
         }
     }
