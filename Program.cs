67319c10c0b8066e99a4089d1eeaca44619c31e1
TEC\HCBS <hans.bode@tecalliance.net>
2023-09-13 09:57:55 +0200

    added string interpolation

--- Program.cs
+++ Program.cs
@@ -12,7 +12,7 @@
             int alter = Convert.ToInt16(age);
             int alterInZehn = alter+ 10;
             
-            Console.WriteLine("Hello, " + name + " Dein Alter ist "+ age + " In Zehn jahren bist du " + alterInZehn);
+            Console.WriteLine($"Hello,   {name} Dein Alter ist  {age}   In Zehn jahren bist du  {alterInZehn}" );
         }
     }
 }
