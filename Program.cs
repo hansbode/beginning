fdc558b84f3933234ada86bb6af32614c14933b4
TEC\HCBS <hans.bode@tecalliance.net>
2023-09-13 09:52:23 +0200

    added age and age in 10 years

--- Program.cs
+++ Program.cs
@@ -6,8 +6,13 @@
         {
             Console.WriteLine("Gebe mir deinen Namen!");
             string name = Console.ReadLine();
+
+            Console.WriteLine("Sage mir wie alt bist du");
+            string age = Console.ReadLine();
+            int alter = Convert.ToInt16(age);
+            int alterInZehn = alter+ 10;
             
-            Console.WriteLine("Hello, " + name);
+            Console.WriteLine("Hello, " + name + " Dein Alter ist "+ age + " In Zehn jahren bist du " + alterInZehn);
         }
     }
 }
