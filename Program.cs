0794d8b42de29751c1e99dfc20b630033f100516
TEC\HCBS <hans.bode@tecalliance.net>
2023-09-13 14:39:08 +0200

    counting from 1 to 9

--- Program.cs
+++ Program.cs
@@ -8,36 +8,11 @@ namespace HelloWorld
     {
         static void Main(string[] args)
         {
-             int zahl1 = 25;
-             int zahl2 = 45;
+             for(int zaehlvariable=0;zaehlvariable<10;zaehlvariable++){
 
-             int ergebnis1 = zahl1 + zahl2;
-             Console.WriteLine(ergebnis1);
-             int ergebnis2 = zahl1 - zahl2;
-             Console.WriteLine(ergebnis2);
-
-             int ergebnis3 = zahl1 * zahl2;
-             Console.WriteLine(ergebnis3);
-
-             int ergebnis4 = zahl1 / zahl1;
-             Console.WriteLine(ergebnis4);
-
-             int ergebnis5 = zahl1*zahl1-zahl1*zahl1;
-             Console.WriteLine(ergebnis5);
-
-             int ergebnis6 = zahl2 % zahl1;
-             Console.WriteLine(ergebnis6);
-
-
-            zahl2++;
-            Console.WriteLine(zahl2);
-
-            zahl1--;
-            Console.WriteLine(zahl1);
-
-             Console.WriteLine(zahl2%zahl1);
-            // C# Operators (w3schools.com)   link para poder hacer tantas operaciones como se me ronque la gana 
-         
+                Console.WriteLine(zaehlvariable);
+             }
+             
 
         }
     }
