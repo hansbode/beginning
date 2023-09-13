4688faf5e9b96158cf4d3829ae07ac8f295a24d7
TEC\HCBS <hans.bode@tecalliance.net>
2023-09-13 11:43:02 +0200

    excercise with differnt types of infos

--- Program.cs
+++ Program.cs
@@ -8,47 +8,29 @@ namespace HelloWorld
     {
         static void Main(string[] args)
         {
-            //CultureInfo cu = new CultureInfo("en-us");                         esto es para que la coma se vuelva punto en idioma gringo
-            //CultureInfo.CurrentCulture = cu;
-            string Text = "Esto es freestyle";
-            Console.WriteLine(Text);
-            int zahl = 17;
-            Console.WriteLine(zahl);
-            double kommazahl =21.3;
-            Console.WriteLine(kommazahl);
-            float weiterFliesKomma = 24.34345345F;
-            Console.WriteLine(weiterFliesKomma); 
-            decimal flieskomma = 26.5M; 
-            Console.WriteLine(flieskomma);
-           
-            bool wahrheitswert =true;
-            Console.WriteLine(wahrheitswert);
-            bool anders = false;
-            Console.WriteLine(anders);
+              
 
-           char buchstabe = (char)42; 
-           Console.WriteLine(buchstabe);
+                 int myNum = 9;         
 
-           long langezahl = 1234567891012345;
-           Console.WriteLine(langezahl);
+                 float myDoubleNum = 8.99f;   
 
-            uint positiveZahlen= 123;
-            Console.WriteLine(positiveZahlen);
+                 char myLetter = 'A';  
 
+                 bool myBoolean = false; 
 
+                 string myText = "Hello World"; 
 
-            string[] myArray = new string[10];
-            myArray[0] = "erster Wert";
-            myArray[9] = "letzter Wert";
-
-            List<string> myList = new List<string>();
             
-            myList.Add("Hans");
-            myList.Add("Samayoa");
+            
+            int zahl = 9;
 
-            Console.WriteLine(myList[0]);
-            Console.WriteLine(myList[1]);
+            double kommazahl = 8.99;
 
+            char buchstabe = 'A'; 
+
+            bool wahrheitswert = false;
+
+            string Text = "Hello World";
         }
     }
 }
