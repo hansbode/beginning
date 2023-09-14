de2b650181756213311f9376d9a0bb6cf4cb7a63
TEC\HCBS <hans.bode@tecalliance.net>
2023-09-14 11:06:42 +0200

    Escribir un texto que siempre sea de 20 letras y las letras que faltan le sume una y

--- Program.cs
+++ Program.cs
@@ -1,4 +1,5 @@
-﻿using System.Globalization;
+﻿using System.ComponentModel;
+using System.Globalization;
 using System.Runtime.CompilerServices;
 using System.Threading.Channels;
 
@@ -8,17 +9,19 @@ namespace HelloWorld
     {
         static void Main(string[] args)
         {
-            //Aufgabe 7: Frage den Nutzer nach einem Text und ersetze im Text alle vorkommenden „U“ und „u“ durch „G“ und „g“
-
+            //Aufgabe 8: Frage den Nutzer nach einer Texteingabe und fülle diese mit bis zu 20 Stellen mit einem y von links auf
+            // escribir un texto que siempre sea de 20 letras y las letras que faltan le sume una y
             Console.WriteLine("Schreibe einen Text");
             string textito = Console.ReadLine();
-            string textito1 = textito.Replace("U", "G");
-            string textito2 = textito1.Replace("u", "g");
+            string texto2 = textito.PadRight(20, 'y');
+            
+             
+           
             
 
 
 
-            Console.WriteLine($"Die neue text Variante lautet: {textito2}");
+            Console.WriteLine($"Die neue text Variante lautet: {texto2} ");
         }
     }
 }
