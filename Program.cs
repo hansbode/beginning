99a83a58b5d79401d6a7704a10b6aa8bd18fd801
TEC\HCBS <hans.bode@tecalliance.net>
2023-09-15 10:23:59 +0200

    Juego de adivinar un numero random 😎

--- Program.cs
+++ Program.cs
@@ -9,134 +9,36 @@ namespace HelloWorld
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Gebe mir  Zahl");
-            string zahl1 =Console.ReadLine();  
-            int numero1 = Convert.ToInt32(zahl1);
-            Console.WriteLine("Gebe mir eine andere Zahl");
-            string zahl2 =Console.ReadLine();  
-            int numero2 = Convert.ToInt32(zahl2);
-            Console.WriteLine("Plus, minus, division, multiplication");
-            string signo = Console.ReadLine();
-
-            
 
 
+            Console.WriteLine("Wähle deine Zahl");
+             int numeroSuerte = Convert.ToInt32(Console.ReadLine());
+            Random random = new Random();
 
-            switch (signo)
+            int randomN = random.Next(0, 101);
+
+
+            while(randomN!=numeroSuerte)
             {
+               
+                if(numeroSuerte>randomN)
+                {
+                    Console.WriteLine($"Deine Nummmer {numeroSuerte} ist größer als die random Nummer");
 
-                case "-":
-                    Substract(numero1,numero2);
-                    break;
+                }
+                else if(numeroSuerte<randomN)
+                {
+                    Console.WriteLine($"Deine Nummer {numeroSuerte} ist kleiner als die random Nummer");
 
-                case "+":
-                    Addition(numero1,numero2);
-                    break;
-
-                case "/":
-                    Division(numero1,numero2);
-                    break;
-
-                case "*":
-                    Multiplicate(numero1,numero2);
-                    break;
-                default:
-                    Console.WriteLine("Wrong operator");
-                    break;
+                }
+                numeroSuerte = Convert.ToInt32(Console.ReadLine());
             }
 
-        }
-
-        static int AdditionWithReturn(string zahl1, string zahl2){
-
-            int numero1 = Convert.ToInt32(zahl1);
-            int numero2 = Convert.ToInt32(zahl2);
-            
-            return numero1 + numero2;
-
-        }
-
-         static int SubstractWithReturn(string zahl1, string zahl2){
-
-            int numero1 = Convert.ToInt32(zahl1);
-            int numero2 = Convert.ToInt32(zahl2);
-            
-            return numero1 - numero2;
-
-        }
-
-
-         static int MultiplicateWithReturn(string zahl1, string zahl2){
-
-            int numero1 = Convert.ToInt32(zahl1);
-            int numero2 = Convert.ToInt32(zahl2);
-            
-            return numero1 * numero2;
-
-        }
-
-
-         static int DivisionWithReturn(string zahl1, string zahl2){
-
-            int numero1 = Convert.ToInt32(zahl1);
-            int numero2 = Convert.ToInt32(zahl2);
-            
-            return numero1 / numero2;
-
-        }
-
-
-
-
-        static void Addition(string zahl1, string zahl2){
-
-            int numero1 = Convert.ToInt32(zahl1);
-            int numero2 = Convert.ToInt32(zahl2);
-            Console.WriteLine(numero1 + numero2);
-
-        }
-
-        static void Substract(string zahl1, string zahl2){
-
-            int numero1 = Convert.ToInt32(zahl1);
-            int numero2 = Convert.ToInt32(zahl2);
-            Console.WriteLine(numero1 - numero2);
-
-        }
-        static void Division(string zahl1, string zahl2){
-
-            int numero1 = Convert.ToInt32(zahl1);
-            int numero2 = Convert.ToInt32(zahl2);
-            Console.WriteLine(numero1 / numero2);
-
-        }
-
-        static void Multiplicate(string zahl1, string zahl2){
-
-            int numero1 = Convert.ToInt32(zahl1);
-            int numero2 = Convert.ToInt32(zahl2);
-            Console.WriteLine(numero1 * numero2);
-
-        }
-
-        static void Addition(int zahl1, int zahl2)         
-        {
-            Console.WriteLine(zahl1 + zahl2);
-        } 
-
-        static void Substract(int zahl1, int zahl2)         
-        {
-            Console.WriteLine(zahl1 - zahl2);
-        } 
-
-        static void Division(int zahl1, int zahl2)         
-        {
-            Console.WriteLine(zahl1 / zahl2);
-        } 
-
-        static void Multiplicate(int zahl1, int zahl2)         
-        {
-            Console.WriteLine(zahl1 * zahl2);
+             if(randomN==numeroSuerte)
+                {
+                        Console.WriteLine($"Du hast gewonnen  mit Nummer {numeroSuerte}");
+                }
+          
         } 
     }
 }
