96bb57b3f5d884e2258c7f58c7172fcaf0061946
TEC\HCBS <hans.bode@tecalliance.net>
2023-09-18 16:23:27 +0200

    Saber cuanta cantidad de una letra tiene la palabra MissiSsiPpi

--- Program.cs
+++ Program.cs
@@ -11,21 +11,18 @@ namespace HelloWorld
         static void Main(string[] args)
         {
 
-          Console.WriteLine("Gebe mir eine Zahl und ich werde bis dieser Zahl die Fibonaccii Reihe machen");
-            int numeroFibo = Convert.ToInt32(Console.ReadLine());
-            int fib1 = 1;
-            int fib2 = 1;
-            int ergebnis = 0;
-            
+          string mis = "MissiSsiPpi";
 
-            while(numeroFibo>(fib1+fib2))
-            {
-                    ergebnis = fib1 +fib2;
-                    Console.WriteLine($"Ergebnis {fib1} + {fib2} = {ergebnis}");
-                    fib1 = fib2;
-                    fib2 = ergebnis;
-            };
-           // 65 = 65,63->0
+          
+            mis = mis.ToLower();
+           int amountM = mis.Count(m => m == 'M' || m == 'm');
+           int amountI = mis.Count(i => i == 'I' || i == 'i');
+           int amountP = mis.Count(p => p == 'P' || p == 'p');
+           int amountS = mis.Count(s => s == 'S' || s == 's');
+          Console.WriteLine($"In Wort {mis} kommt M: {amountM}, I:{amountI};  P:{amountP}, S:{amountS}");
+          
+
+           // contar cuantas veces viene una letra en la oracion 
 
 
 
