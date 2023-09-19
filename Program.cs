b71742ab2fefcd6e79611bf977e2b32dcf0a7fb6
TEC\HCBS <hans.bode@tecalliance.net>
2023-09-19 11:01:59 +0200

    Geschwindigkeit geben und sagen ob es gut oder schlecht ist usw

--- Program.cs
+++ Program.cs
@@ -10,38 +10,72 @@ namespace HelloWorld
     {
         static void Main(string[] args)
         {
-
-          Console.WriteLine("Schreibe ein Text die durch ';' geteilt wird");
-          string texto = Console.ReadLine();
-          texto.Split(";");
-          string[]  myArray = texto.Split(';');
-
-          foreach(string textito in myArray)
-          {
-            Console.WriteLine(texto);
-          }
-
-          for(int index=0; index < myArray.Length; index++)
-          {
-
-            Console.WriteLine(myArray[index]);
-          }
-
-
-          //Split
-
-           
-            
-            
-
-          
-
-           // sacar cuanto tiempo hay, si son 190s sean 3min con 10s
-
-
+        
+          Console.WriteLine("Gebe mir eine Zahl");
+          int Zahl = Convert.ToInt16(Console.ReadLine());
+          string ergebnis = Fahren(Zahl);
+          Console.WriteLine($"Das Auto {ergebnis}");
 
            
         } 
+
+         /*
+            // Passe die Methode "Fahren" so an, dass 
+            //- bei einer Geschwindigkeit von 0 die Methode "steht" zurück gibt: Hinweis: return
+            //- bei einer Geschwindigkeit von 1-10 "schleicht" zurück gibt
+            //- bei einer Geschwindigkeit von 11-50 "fährt langsam" zurück gibt
+            //- bei einer Geschwindigkeit von 51-100 "fährt schnell" zurück gibt
+            //- bei einer Geschwindigkeit über 100 "rast" zurück gibt.
+
+            //Bau die Ausgabe so, dass nach jedem Aufruf folgende Texte ausgegeben werden können. 
+            //Das Auto steht.
+            //Das Auto schleicht.
+            //Das Auto fährt langsam.
+            //Das Auto fährt schnell. 
+            //Das Auto rast.
+            */
+
+        static string Fahren( int geschwindigkeit)
+        {
+            
+            
+            
+
+            if(geschwindigkeit==0)
+            {
+                return "steht";
+            }
+            else if(geschwindigkeit>0 && geschwindigkeit<=10)
+            {
+                return "schleicht";
+            }
+            else if(geschwindigkeit>10 && geschwindigkeit<=50)
+            {
+                return "fährt langsam";
+            }
+            else if(geschwindigkeit>50 && geschwindigkeit<=100)
+            {
+                return "fährt schnell";
+            }
+            else if(geschwindigkeit>100)
+            {
+                return "rast";
+            }
+            else
+            {
+                return "Du hast ja kein Auto";
+
+            }
+
+
+            // geschwindigkeit = 0 "Steht"
+            // geschwindigkeit >0<=10 "schleicht"
+            // geschwindigkeit >10<51 "fährt langsam"
+            // geschwindigkeit >50<101"fährt schnell"
+            //geschwindigkeit>100 " rast"
+
+            
+        }
     }
 }
                     
